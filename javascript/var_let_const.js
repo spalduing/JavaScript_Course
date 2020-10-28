13ba7190aa26c3b0b1e5fcfcaf1cff9e3384ba5a
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-27 20:39:24 -0500

    Differences between var, let and const in thes thre aspects: Function-Scoped or Block-Scoped?, redeclarable?, redifinable?

---
+++ javascript/var_let_const.js
@@ -0,0 +1,56 @@
+// function or block scoped
+
+//var - function scoped
+// for(var i=0; i<5; i++){
+//     console.log(i);
+// }
+
+// console.log("This is not good: " + i);
+
+//let - block scoped
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+    
+// console.log("This is good: " + i);
+
+//const - block scoped
+//                  you cant increment a const.
+// for(const i=0; i<5; i++){
+//         console.log(i);
+//     }
+        
+// ------------------------------------------------
+
+// redeclaring
+
+// var can be redeclared
+// var cat =  "meow";
+
+// var cat  = "pss";
+
+// let cannot be redeclared
+// let dog = "woof";
+
+// let dog = "guau";
+
+
+// const cannot be redeclared
+// const lion = "roarr";
+
+// const lion = "uuhhggg";
+
+// var can be redefined
+// var cat = "meow";
+
+// cat = "Sssss";
+
+// let can be redifined
+// let dog = "Woooff";
+
+// dog = "woff";
+
+//const cannot be redifined
+// const lion = "Roarr";
+
+// lion = "roar";
