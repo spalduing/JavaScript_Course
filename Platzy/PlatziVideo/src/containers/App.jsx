d3b999b82c308cbe40394dd0a84f2cd232fce06a
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-30 18:23:17 -0500

    Containers added, now our components lies within another component that represent a container

---
+++ Platzy/PlatziVideo/src/containers/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header.jsx'
+
+const App = () => (
+    <div className="App">
+        <Header />
+    </div>
+);
+
+export default App; 
--- Platzy/PlatziVideo/src/index.js
+++ Platzy/PlatziVideo/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import Header from './components/Header.jsx';
+import App from './containers/App.jsx';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
