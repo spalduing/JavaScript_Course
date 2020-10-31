ff56e4ecb3deea71523d493b61b843850f08a8c0
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-31 10:38:05 -0500

    Working with React Hooks (its only avaible in the 16.8v). It allow us to give state an life cycle to our function components (aslo know it as Stateless components). Now with reack Hooks its more easy to transmit properties between elements (no matter how far the are) making calls to manage the state and life clycle of the more faraway components

--- Platzy/PlatziVideo/src/components/Footer.jsx
+++ Platzy/PlatziVideo/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../assets/styles/components/Footer.scss'
 
 const Footer = () => (
-    <footer class="footer">
+    <footer className="footer">
     <a href="/">Terminos de uso</a>
     <a href="/">Declaración de privacidad</a>
     <a href="/">Centro de ayuda</a>
--- Platzy/PlatziVideo/src/containers/App.jsx
+++ Platzy/PlatziVideo/src/containers/App.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import Header from '../components/Header.jsx';
-import Search from '../components/Search.jsx';
-import Categories from '../components/Categories.jsx'
-import Carousel  from '../components/Carousel.jsx';
-import Footer from '../components/Footer.jsx';
-import CarouselItem from '../components/CarouselItem'; 
-import '../assets/styles/App.scss';
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header.jsx";
+import Search from "../components/Search.jsx";
+import Categories from "../components/Categories.jsx";
+import Carousel from "../components/Carousel.jsx";
+import Footer from "../components/Footer.jsx";
+import CarouselItem from "../components/CarouselItem";
+import "../assets/styles/App.scss";
 
+const App = () => {
+    
+  const [videos, setVideos] = useState([]);
 
-const App = () => (
+  useEffect(() => {
+      fetch('http://localhost:3000/initalState')
+        .then(response => response.json())
+        .then(data => setVideos(data))
+  }, []);  
+  
+  console.log(videos);
+
+  return (
     <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Header />
+      <Search />
+      <Categories title="Mi Lista">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Categories title="Tendencias">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Originales de Platzivideo"> 
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+      <Categories title="Originales de Platzivideo">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Footer />
-        
+      <Footer />
     </div>
-);
+  );
+}
 
-export default App; 
+export default App;
