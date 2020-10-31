65d41ef393e2c9c8e466fd4a1fd66688a0f93b6c
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-30 21:09:08 -0500

    Structure and Stlyes of Categories, Carousel and CarouselItem

--- Platzy/PlatziVideo/src/containers/App.jsx
+++ Platzy/PlatziVideo/src/containers/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import Header from '../components/Header.jsx'
+import Header from '../components/Header.jsx';
+import Search from '../components/Search.jsx';
+import Categories from '../components/Categories.jsx'
+import Carousel  from '../components/Carousel.jsx';
+import CarouselItem from '../components/CarouselItem'; 
 import '../assets/styles/App.scss';
+
 
 const App = () => (
     <div className="App">
         <Header />
+        <Search />
+        <Categories>
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        
     </div>
 );
 
--- Platzy/PlatziVideo/src/index.js
+++ Platzy/PlatziVideo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header.jsx';
 import App from './containers/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
