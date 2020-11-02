0759c521c8f2c4785e27b328dac5b29b3623f0ba
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-02 10:38:22 -0500

    object.create()

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,6 +1,6 @@
 // JS Objets
 
-// Js Objects initialization
+/////Js Objects initialization////
 
 // let student = {
 //   usn: "114",
@@ -16,21 +16,43 @@
 // console.log("Output is: ", student.firstName);
 // console.log("Output is: ", student.subjects[2]);
 
-// JS Objects constructor
 
-let myLaptop = new Object();
 
-myLaptop.brand = "Dell";
-myLaptop.cores = 8;
-myLaptop["storage"] = "2TB";
+////JS Objects constructor////
 
-console.log(
-  "My laptop info: ",
-  myLaptop.brand,
-  ", Cores: ",
-  myLaptop["cores"],
-  ", Storage: ",
-  myLaptop.storage,
-  ", screen resolution: ",
-  myLaptop.screenResolution // This property/atribute is undefined
-);
+// let myLaptop = new Object();
+
+// myLaptop.brand = "Dell";
+// myLaptop.cores = 8;
+// myLaptop["storage"] = "2TB";
+
+// console.log(
+//   "My laptop info: ",
+//   myLaptop.brand,
+//   ", Cores: ",
+//   myLaptop["cores"],
+//   ", Storage: ",
+//   myLaptop.storage,
+//   ", screen resolution: ",
+//   myLaptop.screenResolution // This property/atribute is undefined
+// );
+
+
+///Object.create()////
+
+const occupation = {
+  type : "none",
+  displayType : function(){
+    console.log(this.type);
+  }
+};
+
+//police
+
+
+let police = Object.create(occupation);
+
+police.type = "police";
+
+
+police.displayType();
