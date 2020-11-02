b04a893606b7e60564b7e38c74ec9c23ccdc68f3
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-02 12:20:04 -0500

    Creating and initializing arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,18 @@
+// Arrays
+
+let arr = [1, 2, 3, 4];
+
+console.log(arr[0]);
+
+let arr1 = new Array(4);
+
+arr1[0] = 10;
+arr1[1] = 20;
+arr1[2] = 30;
+arr1[3] = 40;
+
+console.log(arr1);
+
+let arr2 = new Array("Miguel", "Angel", 3, 4);
+
+console.log(arr2)
