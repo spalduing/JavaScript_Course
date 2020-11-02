e5ac775e0319c46897099c3b8f6795d4360f9eb5
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-02 10:22:02 -0500

    Objects in js, Object initialization and Objects constructor

---
+++ javascript/objects.js
@@ -0,0 +1,34 @@
+// JS Objets
+
+// Js Objects initialization
+
+// let student = {
+//   usn: "114",
+//   firstName: "Miguel",
+//   lastName: "Diaz",
+//   greet: function () {
+//     return "Hello guys";
+//   },
+//   subjects : ["Web Dev", "AI", "blockchain"]
+// };
+
+// console.log("Output is: ", student.greet());
+// console.log("Output is: ", student.firstName);
+// console.log("Output is: ", student.subjects[2]);
+
+// JS Objects constructor
+
+let myLaptop = new Object();
+
+myLaptop.brand = "Dell";
+myLaptop.cores = 8;
+myLaptop.storage = "2TB";
+
+console.log(
+  "My laptop info: ",
+  myLaptop.brand,
+  ", Cores: ",
+  myLaptop["cores"],
+  ", Storage: ",
+  myLaptop.storage
+);
