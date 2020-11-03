f97aab486b2ed933f1cdc81e7a5b71deed5020b6
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-03 14:12:01 -0500

    getters and setters

--- javascript/classes.js
+++ javascript/classes.js
@@ -2,7 +2,16 @@
 class Desert {
   constructor(calories = 100) {
     console.log("This dessert has ", calories.toString(), " calories");
+    this.ingredients = [];
     this.calories = calories;
+  }
+  //setter function
+  addIngredients(ingredient) {
+    this.ingredients.push(ingredient);
+  }
+  //getter function
+  getIngredients() {
+    console.log(this.ingredients);
   }
 }
 
@@ -14,3 +23,15 @@ const strawBerries = new Desert(234);
 
 console.log(strawBerries.calories);
 
+vanilla.addIngredients("Vanilla cloves");
+vanilla.addIngredients("milk");
+vanilla.addIngredients("suggar");
+
+vanilla.getIngredients();
+
+strawBerries.addIngredients("strawberries");
+strawBerries.addIngredients("vanilla cloves");
+strawBerries.addIngredients("milk");
+strawBerries.addIngredients("suggar");
+
+strawBerries.getIngredients();
