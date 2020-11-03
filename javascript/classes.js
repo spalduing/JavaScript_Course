eb71efa0d6dc5b544a9ed01dcd042c1d793eccc1
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-03 13:54:45 -0500

    Using the this. keyword and instanced variables

--- javascript/classes.js
+++ javascript/classes.js
@@ -1,8 +1,16 @@
 //// CLASSSES ////
 class Desert {
-  constructor(calories=100) {
+  constructor(calories = 100) {
     console.log("This dessert has ", calories.toString(), " calories");
+    this.calories = calories;
   }
 }
 
-const vanilla = new Desert(500);
+const vanilla = new Desert(123);
+
+console.log(vanilla.calories);
+
+const strawBerries = new Desert(234);
+
+console.log(strawBerries.calories);
+
