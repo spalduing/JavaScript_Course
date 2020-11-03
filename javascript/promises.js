5634616ab538630ad64c77bdbb0a432425ed61fa
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-03 18:24:42 -0500

    Simulating a request, chaining of .then

--- javascript/promises.js
+++ javascript/promises.js
@@ -2,20 +2,45 @@
 
 let certificatePromise = new Promise((resolve, reject) => {
   // some processing happens and canditate gets certificate
-  certified = false;
+  certified = true;
+
   if (certified) {
-    //success
-    resolve("Certificate obtained!");
+    setTimeout(() => {
+      //success
+      resolve("Certificate obtained!");
+    }, 3000);
   } else {
     //failure
     reject("Certificate rejected!");
   }
 });
 
+let jobPromise = new Promise((resolve, reject) => {
+  // some processing happens and canditate gets certificate
+  jobObtained = true;
+
+  if (jobObtained) {
+    //success
+    resolve("Job obtained!");
+  } else {
+    //failure
+    reject("Job rejected!");
+  }
+});
+
 certificatePromise
+  .then((successResult) => {
+    console.log(successResult);
+    return jobPromise;
+  })
+  .catch((failureResult) => {
+    console.log(failureResult);
+  })
   .then((successResult) => {
     console.log(successResult);
   })
   .catch((failureResult) => {
     console.log(failureResult);
   });
+
+console.log("Rest of the code");
