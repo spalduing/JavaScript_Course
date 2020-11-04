5516a9c45421ed2059de1eed5802996c1bf1cf7c
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-04 17:59:47 -0500

    Using the e event

--- Stickit_notes_project/myscripts.js
+++ Stickit_notes_project/myscripts.js
@@ -1,5 +1,23 @@
-function buttonClicked() {
-  alert("The button is clicked!");
-}
+// function buttonClicked() {
+//   alert("The button is clicked!");
+// }
 
-document.getElementById("test").addEventListener("click", buttonClicked);
+// document.getElementById("test").addEventListener("click", buttonClicked);
+
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+    e.preventDefault();
+  });
+
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  console.log(e.target.type);
+});
+
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  console.log(`(${e.clientX}, ${e.clientY})`);
+});
+
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  console.log(e.shiftKey);
+});
+
+
